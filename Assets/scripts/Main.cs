28e2296ef737cac0bd4a21379a2abb7951cface0
metinsay <metinnsay@gmail.com>
2018-02-21 15:57:35 -0500

    Main File

---
+++ Assets/scripts/Main.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+public class Main : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+        string[] words = System.IO.File.ReadAllLines(@"Assets/data/dictionary.txt");
+        string[] letters = System.IO.File.ReadAllLines(@"Assets/data/letters.txt");
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
